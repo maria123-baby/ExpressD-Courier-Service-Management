22251f68128789e47cac381214ba5950e8f809a8
maria123-baby <145011381+maria123-baby@users.noreply.github.com>
2024-04-10 15:52:03 +0530

    changinlogin

---
+++ src/app/core/models/common.model.ts
@@ -0,0 +1,13 @@
+export interface IUser{
+    key?: string;
+    package_id:string;
+    sender_name: string;
+    sender_address: string;
+    pincode_sender:string;
+    sender_email:string;
+    sender_contact: string;
+    receiver_name: string;
+    receiver_address: string;
+    pincode_receiver: string;
+    receiver_email:string;
+}
