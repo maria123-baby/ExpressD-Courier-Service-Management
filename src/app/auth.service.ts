11572d544ac2049e8d7852beaaa01445c14dd96a
maria123-baby <145011381+maria123-baby@users.noreply.github.com>
2024-04-10 15:46:53 +0530

    Update auth.service.ts

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,18 +1,15 @@
 <<<<<<< HEAD
 import { Injectable, OnInit} from "@angular/core";
 import { AngularFireAuth } from "@angular/fire/compat/auth";
-=======
 import { Injectable, inject, signal} from "@angular/core";
 import { Auth, createUserWithEmailAndPassword, signOut, user } from "@angular/fire/auth";
 import { signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { Observable, from } from "rxjs";
 import { UserInterface } from "./user.interface";
->>>>>>> 79c2a90a4eb9af1f66b697caa03ea4a27f9cf4b3
 
 @Injectable({
     providedIn:'root'
 })
-<<<<<<< HEAD
 export class AuthService implements OnInit{
 
 isAuthenticated:boolean=false;
@@ -25,28 +22,4 @@ constructor(private auth:AngularFireAuth){}
 
  }
     
-=======
-export class AuthService{
-    firebaseAuth = inject(Auth);
-    user$ = user(this.firebaseAuth);
-    currentUserSig = signal<UserInterface | null | undefined>(undefined)
-    register(email:string,username:string,password:string):Observable<void>{
-        const promise = createUserWithEmailAndPassword(this.firebaseAuth,
-            email,
-            password,
-        ).then(response => updateProfile(response.user,{displayName:username}))
-        return from(promise);
-    }
-    login(email:string,password:string):Observable<void>{
-        const promise = signInWithEmailAndPassword(this.firebaseAuth,
-            email,
-            password,
-        ).then(()=>{});
-        return from(promise);
-    }
-    logout():Observable<void>{
-        const promise =signOut(this.firebaseAuth);
-        return from(promise);
-    }
-}
->>>>>>> 79c2a90a4eb9af1f66b697caa03ea4a27f9cf4b3
+
